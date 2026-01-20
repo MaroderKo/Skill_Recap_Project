e4709e1ca6e82b1666d9d41f1d3062ce52bdcb45
perepelytsia.a <perepelytsia.artom@gmail.com>
2026-01-20 16:58:31 +0200

    Initial commit

---
+++ src/main/java/com/dex/srp/SkillRecapProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dex.srp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillRecapProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillRecapProjectApplication.class, args);
+    }
+
+}
