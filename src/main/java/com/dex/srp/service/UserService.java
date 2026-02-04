678eb4a373455585c90ee0f9f1d1924514057121
perepelytsia.a <perepelytsia.artom@gmail.com>
2026-02-04 19:21:20 +0200

    SRB-5: Mark all UserService public methods as Transactional

--- src/main/java/com/dex/srp/service/UserService.java
+++ src/main/java/com/dex/srp/service/UserService.java
@@ -18,6 +18,10 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private User getUserOrThrow(long id)
+    {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+    }
 
     /**
      * Saves user.
@@ -25,13 +29,9 @@ public class UserService {
      * @param userDto user data
      * @return created user
      */
+    @Transactional
     public User save(UserDto userDto) {
-        return save(new User(null, userDto.email(), userDto.username(), userDto.age()));
-    }
-
-    public User save(User user) {
-        log.info("save user with email : {}", user.getEmail());
-        return userRepository.save(user);
+        return userRepository.save(new User(null, userDto.email(), userDto.username(), userDto.age()));
     }
 
     /**
@@ -39,8 +39,8 @@ public class UserService {
      *
      * @return list of all users
      */
+    @Transactional(readOnly = true)
     public List<User> findAll() {
-        log.info(">findAll function of UserService");
         return userRepository.findAll();
     }
 
@@ -51,8 +51,9 @@ public class UserService {
      * @return existing user
      * @throws UserNotFoundException when user with given id does not exist
      */
+    @Transactional(readOnly = true)
     public User findById(long id) {
-        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+        return getUserOrThrow(id);
     }
 
     /**
@@ -65,17 +66,14 @@ public class UserService {
      */
     @Transactional
     public User update(long id, UserDto dto) {
-        User user = findById(id);
-        if (dto.email() != null) {
-            user.setEmail(dto.email());
-        }
+        User user = getUserOrThrow(id);
         if (dto.username() != null) {
             user.setUsername(dto.username());
         }
         if (dto.age() != null) {
             user.setAge(dto.age());
         }
-        return save(user);
+        return user;
     }
 
     /**
@@ -83,8 +81,9 @@ public class UserService {
      *
      * @param id user identifier
      */
+    @Transactional
     public void delete(long id) {
-        User user = findById(id);
+        User user = getUserOrThrow(id);
         userRepository.delete(user);
     }
 
