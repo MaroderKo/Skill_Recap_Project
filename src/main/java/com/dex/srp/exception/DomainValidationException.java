9f388215fd887ce6216b5ea9fc7b9c7faf71eb41
perepelytsia.a <perepelytsia.artom@gmail.com>
2026-02-07 14:28:13 +0200

    SRB-5: AS DomainValidationException contains message only for 1 field, removed duplication message field

--- src/main/java/com/dex/srp/exception/DomainValidationException.java
+++ src/main/java/com/dex/srp/exception/DomainValidationException.java
@@ -5,11 +5,9 @@ import lombok.Getter;
 @Getter
 public class DomainValidationException extends RuntimeException {
     private final String field;
-    private final String message;
 
     public DomainValidationException(String field, String message) {
         super(message);
         this.field = field;
-        this.message = message;
     }
 }
